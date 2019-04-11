50f598f3e45da61af8efee99160de382c884cd3a
yuwen4514 <48576822+yuwen4514@users.noreply.github.com>
2019-04-12 00:46:05 +0800

    Add files via upload

---
+++ basic-homework10.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+main()
+
+{
+
+  char S[5000];
+
+  char ouput;
+
+  printf("輸入一個英文字串："); 
+
+  while(scanf("%s",S)==1) 
+
+  {
+
+   printf("處理過的字串為：",ouput);
+
+   int a,N=strlen(S); 
+   for(a=0;a<N;a++)
+
+   if(S[a]>='a')  
+   printf("%c",S[a]-'a'+'A'); 
+
+   else 
+   printf("%c",S[a]-'A'+'a'); 
+
+  } 
+
+system("pause");
+return 0;  
+
+}
